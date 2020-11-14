97fc48ee5c9bd1e1d66e5f25075401147e0e3f85
martinyounghoonkim <hoons0131@gmail.com>
2020-11-14 22:32:45 +0900

    Add custom eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,9 +24,21 @@ module.exports = {
     "@typescript-eslint"
   ],
   "rules": {
+    "no-console": "warn",
+    "indent": ["error", 2],
+    "arrow-parens": ["error", "as-needed"],
+    "brace-style": ["error", "1tbs"],
     "array-element-newline": ["error", "consistent"],
     "react/prop-types": 0,
+    "semi": ["error", "always"],
     "@typescript-eslint/explicit-module-boundary-types": 0,
     "react/display-name": 0,
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "never",
+      "exports": "never",
+      "functions": "never"
+    }]
   }
 };
